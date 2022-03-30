3dc6455358314c010e01be61479539636a46df69
bill <elsf3458@gmail.com>
2022-03-30 15:10:16 +0800

    test

--- backend/cfehome/djused/views.py
+++ backend/cfehome/djused/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
 import json
+
 # Create your views here.
 from django.http import JsonResponse
+from django.shortcuts import render
+
 
 def api_home(request,*args,**kwargs):
     body = request.body
     print("bodybody",body)
     data=json.loads(body)
+
+    data['headers']=request.headers
+    data['content_type']=request.content_type
+
     print(data)
-    return JsonResponse({"return message":"NEW MESSAGE"})
+    return JsonResponse({"return message":"NEW MESSAGE"+data["WW"]})
