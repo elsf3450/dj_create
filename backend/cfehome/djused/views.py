8aa648b6aa803e8d42cf300e395f02ca23d8870d
bill <elsf3458@gmail.com>
2022-04-19 11:20:04 +0800

    test0419

--- backend/cfehome/djused/views.py
+++ backend/cfehome/djused/views.py
@@ -6,7 +6,11 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 from dj_product.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(["GET","POST"])
 def api_home(request,*args,**kwargs):
     '''
     body = request.body
@@ -20,10 +24,18 @@ def api_home(request,*args,**kwargs):
     print("A_A",type(json.dumps(dict(request.headers))))
     print(data)
     '''
+    body = request.body
+    print('bodybodybody0,body',body)
+    print("request.headers",request.GET)
     model_data=Product.objects.all().order_by("?").first()
     data = {}
+    '''
+    data['id']=model_data.id
     data["title"] = model_data.title
     data["content"] = model_data.contect
     data["price"] = model_data.price
-    return JsonResponse(data)
+    '''
+    data=model_to_dict(model_data,fields=['id','title'])
+    return Response(data)
+    #return Response(data,status=405)
     #return JsonResponse({"return message":"NEW MESSAGE"+data["WW"]})
--- client/basic.py
+++ client/basic.py
@@ -4,4 +4,4 @@ endpoint='http://127.0.0.1:8000/djused35/'
 #aa=(requests.get(endpoint,params={"data_byget":"WOWO"},json={"WW":'e04'})).json()
 aa=(requests.get(endpoint,params={"data_byget":"WOWO"},json={"WW":'e04'}))
 print(aa.json())
-print(type(aa))
+print(type(aa.json()))
